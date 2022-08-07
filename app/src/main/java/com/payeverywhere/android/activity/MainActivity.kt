8955d6968df86e218011ff9151941273e4ec110c
tzemin <tzemin95@gmail.com>
2022-08-07 12:28:40 +0800

    Create MainActivity.kt Create new model (User and Transaction)

---
+++ app/src/main/java/com/payeverywhere/android/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.payeverywhere.android.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.payeverywhere.android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
